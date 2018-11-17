3b685fbed35615396b0f14d29959693e52559fef
Jakub Binkowski <jakubin@gmail.com>
2018-11-17 11:15:27 +0100

    Wip

--- L01/myapp/src/App.js
+++ L01/myapp/src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "John", age: 22 }
+    ]
+  };
+
+  switchNameHandler = () => {
+    alert('boom');
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hello, Dupa</h1>
-        <Person name="Max" age="28"/>
-        <Person name="John" age="22">My hobbies: ski</Person>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: ski</Person>
       </div>
     );
   }

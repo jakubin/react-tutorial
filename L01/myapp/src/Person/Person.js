69ced2363cc07d283d561034e4ced35e22d02ec6
Jakub Binkowski <jakubin@gmail.com>
2018-11-17 19:49:56 +0100

    WIP

--- L01/myapp/src/Person/Person.js
+++ L01/myapp/src/Person/Person.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
+    const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px'
+    };
     return (
-        <div>
-            <p>I'm a {props.name} and I am {props.age} years old! </p>
+        <div className="Person">
+            <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old! <span onClick={props.deleteClicked}>X</span> </p>
             <p>{props.children}</p>
+            <input style={style} type="text" onChange={props.nameChanged} value={props.name}/>
         </div>
     );
 };
